33e52f1b28c6c29dab793995d00d370dc7e22a20
huayifeng <huayifeng@u51.com>
2017-07-23 11:23:29 +0800

    fix bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,15 +12,6 @@ module.exports = {
 	],
 	// add your custom rules here
 	'rules': {
-		// allow paren-less arrow functions
-		'arrow-parens': 0,
-		// allow async-await
-		'generator-star-spacing': 0,
-		// allow debugger during development
-		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-		'semi': ['error', 'always'],
-		'indent': 0,
-		'space-before-function-paren': 0,
-    "no-multiple-empty-lines": [1, {"max": 2}]
+
 	}
 }
