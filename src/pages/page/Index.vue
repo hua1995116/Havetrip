fb2afe304e11b29041abe1d6a9e6acb61300d136
huayifeng <huayifeng@u51.com>
2017-07-29 18:13:32 +0800

    增加接口

--- src/pages/page/Index.vue
+++ src/pages/page/Index.vue
@@ -6,17 +6,35 @@
       <li><router-link :to="{ path: '/index/detail3' }">tab3</router-link></li>
     </ul>
     <div class="index-l">
+      <div>
+        {{list}}
+      </div>
       <router-view></router-view>
     </div>
   </div>
 </template>
 <script>
   import Abstract from '../common/Abstract.vue'
+  import axios from 'axios'
   export default {
     mixins: [Abstract],
     doNotInit: true,
+    data () {
+      return {
+        list: []
+      }
+    },
     created () {
       this.init()
+      this.ajaxgetdata()
+    },
+    methods: {
+      ajaxgetdata () {
+        axios.get('/api/ssm01/country/oneCountryById?id=1')
+          .then((res) => {
+            this.list = res.data
+          })
+      }
     }
   }
 </script>
