d19c4847861458eb8dd1ed44703d8711cbb3e547
huayifeng <huayifeng@u51.com>
2017-07-31 15:55:18 +0800

    add editor

--- src/pages/page/Notes.vue
+++ src/pages/page/Notes.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
     notes
+    <vue-html5-editor @change="updateData" :content="content" :height="500" :z-index="1000" :auto-height="true" ></vue-html5-editor>
   </div>
 </template>
 <script>
   export default {
+    data () {
+      return {
+        content: 'hello'
+      }
+    },
+    methods: {
+      updateData (data) {
+        console.log(data)
+      }
+    }
   }
 </script>
 <style scoped lang="stylus">
